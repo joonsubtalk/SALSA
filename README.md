09966edc9bc2bb5996efc9a44c13379fc9536ee8
Joon <joonsubtalk@gmail.com>
2014-04-28 23:05:36 -0700

    Small updates

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 SALSA
 =====
 
-Strand ALigned Sequence Assembler
+Strand ALigning Sequence Assembler
 =====
+
+Fragger.java
+Uses Generator to generate a random sequence and then frags them based on the requirement.
 
--- src/com/joonsub/salsa/Node.java
+++ src/com/joonsub/salsa/Node.java
@@ -3,12 +3,24 @@ package com.joonsub.salsa;
 import java.util.List;
 
 public class Node {
+	
+	// make child node
+	private Node[] c;
+	public Node(){
+		c = new Node[4];
+	}
+	
+	public void insert(String s){
+		
+	}
+	
 	private char value;
 	private List<Node> nextNode;
-	
+	/*
 	public Node(){
 		this.value = ' ';
 	}
+	*/
 	public Node(char value){
 		this.value = value;
 	}
